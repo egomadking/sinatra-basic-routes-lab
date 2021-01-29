84ff657714235bfb9a8d233ff05d19d0b1cce34c
Roy Mosby <ego.madking@gmail.com>
2021-01-29 12:12:19 -0500

    commit tree 01c490baaad169f2d5337c46338fd41992347a19

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/name" do
+    status 200
+    body "My name is Roy"
+  end
+
+  get "/hometown" do
+    status 200
+    body "My hometown is Columbia"
+  end
+
+  get "/favorite-song" do
+    status 200
+    body "My favorite song is Put Me to Work"
+  end
 end
